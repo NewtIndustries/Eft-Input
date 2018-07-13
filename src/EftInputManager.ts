2fd201b9d6efd79a23d5b1be32c90cda6f1bd8f7
Christopher Anderson <chanderson@gmrmarketing.com>
2018-07-13 11:40:56 -0500

    Add collections, start to reorganize and test input manager system

--- src/EftInputManager.ts
+++ src/EftInputManager.ts
@@ -1,8 +1,10 @@
 import { Key } from 'ts-keycode-enum';
 import { KeyboardManager, KeyboardState, GamepadManager, MouseManager, TouchManager } from './Sources';
 import * as PubSub from 'pubsub-js';
-import { IInputEvent, InputEventOf } from './Events/InputEvent';
+import { IInputEvent, InputEvent, InputEventOf } from './Events/InputEvent';
 import { Log } from './Log';
+import { GameEvent } from './Events/GameEvent';
+import * as Collections from 'typescript-collections';
 
 export class EftInputManager {
 	private static _instance: EftInputManager;
@@ -31,15 +33,33 @@ export class EftInputManager {
 		this._gamepad = new GamepadManager();
 		this._mouse = new MouseManager();
 		this._touch = new TouchManager();
+
+		this._gameEventSet = new Collections.Set<GameEvent>();
+		this._inputEventSet = new Collections.Set<InputEvent>();
+		this._gameInputEventMap = new Map<InputEvent, GameEvent>();
 		// window.requestAnimationFrame(this.update);
 	}
 	private Update = () => {
 		this._gamepad.Update();
 	}
 
-	public Bind(gameEventConstant: string, handler: Function) {
+	// #region Should be moved into a more automatic mapping system
+	private _gameEventSet: Collections.Set<GameEvent>;
+	private _inputEventSet: Collections.Set<InputEvent>;
+	private _gameInputEventMap: Map<InputEvent, GameEvent>;
+
+	public RegisterGameEvent(gameEvent: GameEvent) {
+		
+	}
+
+	public RegisterInputEvent(inputEvent: InputEvent) {
 
 	}
+
+	public MapGameEventToInputEvent(inputEvent: InputEvent, gameEvent: GameEvent) {
+
+	}
+	// #endregion
 
 	public Subscribe<T = any>(topic: string, callback: (event: IInputEvent<T>) => void): any {
 		var token = PubSub.subscribe(topic, (msg: string, data: any) => {
---
+++ test/index.ts
@@ -0,0 +1,17 @@
+import { EftInputManager } from '../src/EftInputManager';
+
+const TestGameEvents = {
+	MOVE_FORWARD: 'MOVE.FORWARD'
+}
+
+export class TestClient {
+	constructor() {
+		var iManager = EftInputManager.Instance;
+
+		// Register Game Events
+		
+		// Register Input Events
+		// Map Game Events to Input Events
+
+	}
+}
